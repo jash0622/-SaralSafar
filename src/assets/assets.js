29616c71a45e369c0f8df326ec411f0c58fb743b
jash0622 <jash22bh@gmail.com>
2024-09-20 10:50:44 +0530

    Create assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,42 @@
+import facebook_icon from './facebook_icon.png';
+import twitter_icon from './twitter_icon.png';
+import linkedin_icon from './linkedin_icon.png';
+import logo from './logo.png'
+import app_store from './app_store.png'
+import bus_tracking from './bus-tracking.png'
+import busTrack from './busTrack.png'
+import liveCount from './liveCount.png'
+import compRoute from './compRoute.png'
+import LiveTrack from './LiveTrack.png'
+import LivePasse from './LivePasse.png'
+import RouteInfo from './RouteInfo.png'
+import logo1 from './logo (2).png'
+import HeaderBus from './HeaderBus.png'
+import Location from './Location.png'
+import Locate from './Locate.png'
+import LiveTracking from './LiveTracking.png'
+import BusInfo from './BusInfo.png'
+import BusMove from './BusMove.png'
+
+
+export const assets = {
+    app_store,
+    BusMove,
+    BusInfo,
+    LiveTracking,
+    Locate,
+    Location,
+    HeaderBus,
+    logo1,
+    RouteInfo,
+    LivePasse,
+    LiveTrack,
+    compRoute,
+    liveCount,
+    busTrack,
+    bus_tracking,
+    logo,
+  facebook_icon,
+  twitter_icon,
+  linkedin_icon,
+};
